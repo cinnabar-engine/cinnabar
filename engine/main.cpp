3f0ef03b543b1e146e4eb9195b43936c1e3124cc
Tumble <tumble@tumble-pc>
2021-02-15 11:35:38 +0000

    wasd movement

--- engine/main.cpp
+++ engine/main.cpp
@@ -116,6 +116,10 @@ int main(int argc, char* argv[]) {
 	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	
+	float
+		deltaTime = 0.0f,
+		last = 0.0f;
 
 	ce::Shader* shader = new ce::Shader("basic");
 	
@@ -127,7 +131,19 @@ int main(int argc, char* argv[]) {
 	ce::Texture* texture = new ce::Texture("uv-map.png");
 	shader->setInt("fText",0);
 	
-	glm::mat4 proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+	
+	glm::vec3
+		cameraPos(0.0f, 0.0f, 80.0f),
+		cameraFront(0.0f,0.0f,-1.0f),
+		cameraUp(0.0f,1.0f,0.0f);
+		//cameraTarget(0.0f,0.0f,0.0f);
+		//cameraDirection = glm::normalize(cameraPos - cameraTarget),
+		//up(0.0f,1.0f,0.0f);
+		//cameraRight = glm::normalize(glm::cross(up,cameraDirection));
+		//cameraUp = glm::cross(cameraDirection,cameraRight);
+	
+	glm::mat4 proj = glm::perspective(glm::radians(45.0f), WINDOW.x / WINDOW.y, 0.1f, 100.0f);
+	shader->setMat4("uProj",proj);
 		
 	/*
 	 * Game Loop
@@ -136,26 +152,51 @@ int main(int argc, char* argv[]) {
 	int running = 1;
 	while (running)
 	{
+		float
+			now = (float)SDL_GetTicks()/1000;
+			deltaTime = now-last;
+			last = now;
+		
 		//Events
 		while (SDL_PollEvent(&event)) {
+			switch(event.type) {
+				case SDL_KEYDOWN:
+					std::cout << event.key.keysym.sym << "\n";
+					const float cameraSpeed = 2.5f*deltaTime; // adjust accordingly
+					if (event.key.keysym.sym == SDLK_w)
+						cameraPos += cameraSpeed * cameraFront;
+					if (event.key.keysym.sym ==  SDLK_s)
+						cameraPos -= cameraSpeed * cameraFront;
+					if (event.key.keysym.sym == SDLK_a)
+						cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+					if (event.key.keysym.sym == SDLK_d)
+						cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+				break;
+			}
 			if (event.type == SDL_QUIT) {
 				running = 0;
 				break;
 			}
-		}
-		float gameTime = (float)SDL_GetTicks()/1000;
-		
+		}		
 		
 		/* Render */
 		glClearColor(0.0f,0.0f,0.0f,1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 		
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
-		glm::mat4 model(1.0f),view(1.0f);
-		model = glm::rotate(model, gameTime * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); 
+		
+		/*float
+			radius = 10.0f,
+			camX = sin(gameTime)*radius,
+			camZ = cos(gameTime)*radius;*/
+		glm::mat4
+			model(1.0f),
+			//view = glm::lookAt(glm::vec3(camX,0.0f,camZ),glm::vec3(0.0f,0.0f,0.0f),glm::vec3(0.0f,1.0f,0.0f));
+			view = glm::lookAt(cameraPos,cameraPos + cameraFront,cameraUp);
+		model = glm::rotate(model, now * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));  
+		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f)); 
+		
 		shader->setMat4("uModel",model);
 		shader->setMat4("uView",view);
-		shader->setMat4("uProj",proj);
 	
 		
 		texture->activate(0);
