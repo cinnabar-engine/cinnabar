63cd7a562cf994366651153b93e9fe091d1c6c6f
Tumble <tumble@tumble-pc>
2021-02-17 00:28:42 +0000

    resolve conflics

--- engine/rendering/camera.cpp
+++ engine/rendering/camera.cpp
@@ -16,14 +16,14 @@ glm::mat4 ce::Camera::getView()
 	glm::vec3
 		pos = m_transform->getPosition(),
 		forward = m_transform->getForward();
-	return glm::lookAt(pos,pos+forward,ce::Transform::getGloablUp());
+	return glm::lookAt(pos,pos+forward,ce::Transform::GetGlobalUp());
 }
 
 glm::vec3 ce::Camera::getRight()
 {
 	glm::vec3
 		forward = m_transform->getForward(),
-		up = ce::Transform::getGloablUp();
+		up = ce::Transform::GetGlobalUp();
 	return glm::normalize(glm::cross(forward,up));
 }
 
@@ -31,3 +31,14 @@ void ce::Camera::sendToShader(ce::Shader* shader)
 {
 	shader->setMat4("transform.view",getView());
 }
+
+void ce::Camera::boundPitch()
+{
+	float cameraPitch = m_transform->getPitch();
+	if (cameraPitch > 89.9f) // TODO: fix broken 90 degree pitch
+		cameraPitch = 89.9f;
+	if (cameraPitch < -89.9f)
+		cameraPitch = -89.9f;
+	m_transform->setPitch(cameraPitch);
+}
+
