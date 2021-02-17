2f4b1ae03141514c2a3564c4fc4f896b90c6c65f
Tumble <tumble@tumble-pc>
2021-02-17 19:12:36 +0000

    plane found

--- engine/main.cpp
+++ engine/main.cpp
@@ -36,10 +36,10 @@ ce::Vertex vertices[] = {
 	glm::vec3(-0.5f,  0.5f, 0.5f), glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), glm::vec2(0.0f, 1.0f),// 7
 };
 ce::Vertex planeVertices[] = {
-	glm::vec3( 10.0f,  -1.0f, -10.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f), glm::vec2(1.0f, 1.0f),// 0
-	glm::vec3( 10.0f, -1.0f, -10.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f), glm::vec2(1.0f, 0.0f),// 1
-	glm::vec3(-10.0f, -1.0f, -10.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.0f),// 2
-	glm::vec3(-10.0f,  -1.0f, -10.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f), glm::vec2(0.0f, 1.0f)// 3
+	glm::vec3( 1.0f,  0.0f, 1.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f), glm::vec2(1.0f, 1.0f),// 0
+	glm::vec3( 1.0f, 0.0f, -1.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f), glm::vec2(1.0f, 0.0f),// 1
+	glm::vec3(-1.0f, 0.0f, -1.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f), glm::vec2(0.0f, 0.0f),// 2
+	glm::vec3(-1.0f,  0.0f, 1.0f), glm::vec4(0.0f, 1.0f, 0.0f, 1.0f), glm::vec2(0.0f, 1.0f)// 3
 };
 // clang-format on
 
