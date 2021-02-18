98c9ed8e508e57adbc1d7ad6f640850e17f5d10e
Tumble <tumble@tumble-pc>
2021-02-18 00:26:19 +0000

    mesh file loading

--- engine/managers/asset_manager.h
+++ engine/managers/asset_manager.h
@@ -9,13 +9,16 @@ namespace ce {
 	 private:
 		inline static const std::string
 			SHADER_FOLDER = "shaders",
-			TEXTURE_FOLDER = "textures";
+			TEXTURE_FOLDER = "textures",
+			MESH_FOLDER = "meshes";
 
 		static std::string load_text_file(std::string file);
 
 	 public:
 		static ShaderFile getShaderFile(std::string filename);
-		static ce::TextureFile getTextureFile(std::string filename);
+		static TextureFile getTextureFile(std::string filename);
 		static void freeTextureFile(ce::TextureFile textureFile);
+		
+		static MeshFile getMeshFile(std::string filename);
 	};
 }
