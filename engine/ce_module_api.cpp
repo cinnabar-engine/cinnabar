3789a40acbc46893ec1cbb3e7b1ad94db657b358
Tumble <tumble@tumble-pc>
2021-02-18 20:51:51 +0000

    created module loading system

---
+++ engine/ce_module_api.cpp
@@ -0,0 +1,9 @@
+/**
+ * Cinnabar Module API
+ * Author: Cameron Trow <tumblegamer@gmail.com>
+ *
+ * Entry point for the version of the module api for use during module development
+ * 
+ */
+
+#include "core/module.h"
