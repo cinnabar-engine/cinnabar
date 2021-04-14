d33e569edb76e9b331b20941b9259f8cbed99a86
SArpnt <36542932+SArpnt@users.noreply.github.com>
2021-04-13 23:07:58 -0400

    cleanup

--- src/ce_render_fundementals.h
+++ src/ce_render_fundementals.h
@@ -1,10 +1,5 @@
-// #include <memory>
+#pragma once
 
 #include "rendering/mesh.h"
 #include "rendering/shader.h"
 #include "rendering/texture.h"
-/*
-#define PTR std::shared_ptr
-#define PTR_CAST = dynamic_pointer_cast
-#define OBJMAP(x) std::map<std::string,PTR<x>>
-*/
