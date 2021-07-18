f012bcdf38f8811a56084393d040f535af5e6e14
SArpnt <36542932+SArpnt@users.noreply.github.com>
2021-07-17 21:44:36 -0400

    remove ce_rendering.h

--- src/rendering/texture.h
+++ src/rendering/texture.h
@@ -1,20 +1,20 @@
 #pragma once
 
+#include <GL/glew.h>
 #include <ce_assets.h>
-#include <ce_rendering.h>
 #include <managers/asset_manager.h>
 
 namespace ce {
 	class Texture {
-	 private:
-		GLuint m_texture;
-		int m_width, m_height, m_channelCount;
-		unsigned int m_type;
-
 	 public:
 		Texture(std::string filename, GLenum type = GL_TEXTURE_2D);
 		~Texture();
 
 		void bind(), unbind(), activate(int slot);
+
+	 private:
+		GLuint m_texture;
+		int m_width, m_height, m_channelCount;
+		unsigned int m_type;
 	};
 }
