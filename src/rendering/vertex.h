211193eef52b0fc1cc51bfec9b2d5bc4c11fcd57
SArpnt <36542932+SArpnt@users.noreply.github.com>
2021-07-18 13:39:21 -0400

    changed vertex properties

--- src/rendering/vertex.h
+++ src/rendering/vertex.h
@@ -5,7 +5,8 @@
 namespace ce {
 	struct Vertex { // TODO: we need a better mesh system, probably just store the raw buffers instead of constantly converting
 		glm::vec3 position;
-		glm::vec4 color;
-		glm::vec2 texCoord;
+		glm::vec3 normal;
+		glm::vec2 uv = glm::vec2(0.0f, 0.0f);
+		glm::vec4 color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 	};
 }
