bc0b8ebbe2668e07da6a55757d5585efc96e975b
SArpnt <36542932+SArpnt@users.noreply.github.com>
2021-07-17 22:36:53 -0400

    log fix and cleanup

--- src/rendering/vertex.h
+++ src/rendering/vertex.h
@@ -2,9 +2,8 @@
 
 #include <ce_math.h>
 
-
 namespace ce {
-	struct Vertex { // TODO: just remove this class entirely, vertexes don't and shouldn't work like this
+	struct Vertex { // TODO: we need a better mesh system, probably just store the raw buffers instead of constantly converting
 		glm::vec3 position;
 		glm::vec4 color;
 		glm::vec2 texCoord;
