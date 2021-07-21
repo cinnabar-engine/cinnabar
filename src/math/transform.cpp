c50bdbccc02d3df0cd27854dd2f78760639ab084
SArpnt <36542932+SArpnt@users.noreply.github.com>
2021-07-20 22:13:07 -0400

    fix transform

--- src/math/transform.cpp
+++ src/math/transform.cpp
@@ -9,20 +9,17 @@ ce::Transform::~Transform() {}
 glm::mat4 ce::Transform::getMatrix() {
 	glm::mat4 transform(1.0f);
 
-	// Scale
-	transform = glm::scale(transform, m_scale);
+	// Translation
+	transform = glm::translate(transform, m_pos);
 
 	// Rotation
 	// TODO: Quaternions
-	transform = glm::rotate(transform, glm::radians(getPitch()),
-		glm::vec3(1.0f, 0.0f, 0.0f)); // Pitch
-	transform = glm::rotate(
-		transform, glm::radians(getYaw()), glm::vec3(0.0f, 1.0f, 0.0f)); // Yaw
-	transform = glm::rotate(
-		transform, glm::radians(getRoll()), glm::vec3(0.0f, 0.0f, 1.0f)); // Roll
+	transform = glm::rotate(transform, glm::radians(getPitch()), glm::vec3(1.0f, 0.0f, 0.0f)); // Pitch
+	transform = glm::rotate(transform, glm::radians(getYaw()), glm::vec3(0.0f, 1.0f, 0.0f)); // Yaw
+	transform = glm::rotate(transform, glm::radians(getRoll()), glm::vec3(0.0f, 0.0f, 1.0f)); // Roll
 
-	// Translation
-	transform = glm::translate(transform, m_pos);
+	// Scale
+	transform = glm::scale(transform, m_scale);
 
 	return transform;
 }
