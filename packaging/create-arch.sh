fc147fc1be91906298c5fa87ac2ae48ca262889f
Tumble <tumblegamer@gmail.com>
2021-08-15 00:42:06 +0100

    arch creating

---
+++ packaging/create-arch.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+function prep_arch { #(TARGET)
+TARGET=$1
+NAME=cinnabar-$1
+
+PAKNAME=$NAME
+ARCH=./arch/$TARGET
+
+LIB=./build/run/lib$PAKNAME.so
+INCLUDE=./src/cinnabar-engine/$TARGET
+
+PKGROOT=./packaging/$PAKNAME/pkg/$PAKNAME
+
+if [ ! -f ${LIB} ]
+then
+	cmake --build ./build --target ${NAME}
+fi
+
+rm -rf packaging/${PAKNAME}
+mkdir -p packaging/${PAKNAME}/include
+
+cp -r ${ARCH}/* packaging/${PAKNAME}
+
+# runtime
+cp ${LIB} packaging/${PAKNAME}
+
+#dev
+cp -r ${INCLUDE}/*.hpp packaging/${PAKNAME}/include
+cp -r ${INCLUDE}/*.h packaging/${PAKNAME}/include
+rm packaging/${PAKNAME}/include/stb_image.h
+}
+
+function apkg-arch {
+	cd $1
+	makepkg
+	cd ..
+}
+
+cd $(dirname $0)/..
+if [ ! -d ./build ]
+then
+	mkdir build
+	cd build
+	cmake ..
+	cd ..
+fi
+
+prep_arch core
+prep_arch render
+
+cd $(dirname $0)
+
+apkg-arch cinnabar-core
+apkg-arch cinnabar-render
+cp */*.pkg.tar.zst .
