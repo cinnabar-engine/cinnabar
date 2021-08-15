3fec3c8eb35075fe71c2744e4b87aa050d2a9fcb
Tumble <tumblegamer@gmail.com>
2021-08-15 15:10:54 +0100

    finishing touches

--- packaging/create-deb.sh
+++ packaging/create-deb.sh
@@ -4,36 +4,36 @@ cd $(dirname $0)/..
 
 
 
-function create_deb { #(TARGET)
-TARGET=$1
-NAME=cinnabar-$1
+function prep_deb { #(TARGET)
+	TARGET=$1
+	NAME=cinnabar-$1
 
-PAKNAME=lib$NAME
-DEBIAN=./debian/$TARGET/runtime
+	PAKNAME=lib$NAME
+	DEBIAN=./debian/$TARGET/runtime
 
-PAKNAMEDEV=$PAKNAME-dev
-DEBIANDEV=./debian/$TARGET/dev
+	PAKNAMEDEV=$PAKNAME-dev
+	DEBIANDEV=./debian/$TARGET/dev
 
-LIB=./build/run/$PAKNAME.so
-INCLUDE=./src/cinnabar-engine/$TARGET
+	LIB=./build/run/$PAKNAME.so
+	INCLUDE=./src/cinnabar-engine/$TARGET
 
-if [ ! -f ${LIB} ]
-then
-	cmake --build ./build --target ${NAME}
-fi
-rm -rf packaging/${PAKNAME}
-rm -rf packaging/${PAKNAMEDEV}
-mkdir -p packaging/${PAKNAME}/{DEBIAN,usr/lib}
-mkdir -p packaging/${PAKNAMEDEV}/{DEBIAN,usr/include/$NAME}
+	if [ ! -f ${LIB} ]
+	then
+		cmake --build ./build --target ${NAME}
+	fi
+	rm -rf packaging/${PAKNAME}
+	rm -rf packaging/${PAKNAMEDEV}
+	mkdir -p packaging/${PAKNAME}/{DEBIAN,usr/lib}
+	mkdir -p packaging/${PAKNAMEDEV}/{DEBIAN,usr/include/$NAME}
 
-cp -r ${DEBIAN}/* packaging/${PAKNAME}/DEBIAN
-cp ${LIB} packaging/${PAKNAME}/usr/lib
+	cp -r ${DEBIAN}/* packaging/${PAKNAME}/DEBIAN
+	cp ${LIB} packaging/${PAKNAME}/usr/lib
 
 
-cp -r ${DEBIANDEV}/* packaging/${PAKNAMEDEV}/DEBIAN
-cp -r ${INCLUDE}/*.hpp packaging/${PAKNAMEDEV}/usr/include/${NAME}
-cp -r ${INCLUDE}/*.h packaging/${PAKNAMEDEV}/usr/include/${NAME}
-rm packaging/${PAKNAMEDEV}/usr/include/${NAME}/stb_image.h
+	cp -r ${DEBIANDEV}/* packaging/${PAKNAMEDEV}/DEBIAN
+	cp -r ${INCLUDE}/*.hpp packaging/${PAKNAMEDEV}/usr/include/${NAME}
+	cp -r ${INCLUDE}/*.h packaging/${PAKNAMEDEV}/usr/include/${NAME}
+	rm packaging/${PAKNAMEDEV}/usr/include/${NAME}/stb_image.h
 
 }
 
@@ -53,7 +53,8 @@ cd packaging
 rm *.deb
 for a in "./"*/
 do
-dpkg-deb --build $(basename $a)
+echo "$a: $(basename $a)"
+	dpkg-deb --build $(basename $a)
 done
 rm -rf */
 cd ..
