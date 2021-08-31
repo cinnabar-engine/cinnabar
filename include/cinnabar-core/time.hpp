bace5f32915c32426bdc05916af680c241d4c7b1
Tumble <tumblegamer@gmail.com>
2021-08-31 16:48:30 +0100

    cinnabar core-documentation

--- include/cinnabar-core/time.hpp
+++ include/cinnabar-core/time.hpp
@@ -3,14 +3,47 @@
 namespace ce {
 	class Time {
 	 public:
+		/**
+	  * @brief Construct a new Time object
+	  * 
+	  */
 		Time();
 		~Time();
-
+		/**
+		 * @brief Calculates the FPS and DeltaTime
+		 * 
+		 * @param dt weather to calculate the DeltaTime
+		 * @param fps weather to calculate the FPS
+		 */
 		void recalculate(bool dt = true, bool fps = true);
+
+		/**
+		 * @brief Updates the FPS and DeltaTime and makes a GameTick
+		 * 
+		 * @param dt weather to calculate the DeltaTime
+		 * @param fps weather to calculate the FPS
+		 */
 		void update(bool dt = true, bool fps = true);
+
+		/**
+		 * @brief Wait until delta reches a certain values
+		 * 
+		 * @param dt value to wait for
+		 */
 		void waitUntilDelta(double dt);
 
+		/**
+		 * @brief Get the Delta Time
+		 * 
+		 * @return double 
+		 */
 		double getDeltaTime() { return m_dt; }
+
+		/**
+		 * @brief Get the FPS
+		 * 
+		 * @return double 
+		 */
 		double getFPS() { return m_fps; };
 
 	 private:
