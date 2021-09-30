688e68aea9db530b02a95bd9bf5f6729ff1c6625
SArpnt <36542932+SArpnt@users.noreply.github.com>
2021-09-30 10:56:10 -0400

    change amount of windows in demo

--- src/cinnabar-render-demo/main.cpp
+++ src/cinnabar-render-demo/main.cpp
@@ -13,13 +13,40 @@
 
 namespace demo {
 	ce::Time* time = NULL;
-	ce::Window* window1 = NULL;
-	ce::Window* window2 = NULL;
+	typedef struct {
+		ce::Window* window;
+		ce::Camera* camera;
+	} WinCam;
+	std::vector<WinCam> wincams = {};
 	ce::RenderEngine* renderEngine = NULL;
-	ce::Camera* camera1 = NULL;
-	ce::Camera* camera2 = NULL;
+	ce::Transform* cameraTransform = NULL;
 	glm::vec3 cameraVelocity;
 	double mouseSens = 0.05;
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void cursorPositionCallback(GLFWwindow* window, double xpos, double ypos);
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
+void windowSizeCallback(GLFWwindow* window, int width, int height);
+
+void makeWindow() {
+	ce::Window* window = new ce::Window(("Cinnabar " + std::to_string(demo::wincams.size())).c_str());
+	window->makeCurrent();
+	demo::renderEngine->vsync(0);
+	demo::renderEngine->setFramebufferSize(window->getFramebufferSize());	
+	double deltaTimeMin = 1.0 / 1000.0; // framerate cap
+
+	if (glfwRawMouseMotionSupported()) // TODO: make function for all these GLFW functions and figure out for multiple windows
+		window->setInputMode(GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+	glfwSetKeyCallback(window->getWindow(), keyCallback);
+	glfwSetCursorPosCallback(window->getWindow(), cursorPositionCallback);
+	glfwSetMouseButtonCallback(window->getWindow(), mouseButtonCallback);
+	glfwSetWindowSizeCallback(window->getWindow(), windowSizeCallback);
+
+	ce::Camera* camera = new ce::Camera(demo::cameraTransform);
+	camera->projection = glm::perspective(glm::radians(75.0), (double)window->getWindowAspectRatio(), 0.1, 100.0);
+
+	demo::wincams.push_back({window, camera});
 }
 
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) { // TODO: get window class from callback instead of GLFWwindow
@@ -49,14 +76,18 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 					break;
 
 				case GLFW_KEY_Q:
-					demo::camera1->transform->roll(cameraRollSpeed);
+					demo::cameraTransform->roll(cameraRollSpeed);
 					break;
 				case GLFW_KEY_E:
-					demo::camera1->transform->roll(-cameraRollSpeed);
+					demo::cameraTransform->roll(-cameraRollSpeed);
 					break;
 
 				case GLFW_KEY_ESCAPE:
-					demo::window1->setInputMode(GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+					demo::wincams[0].window->setInputMode(GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+					break;
+
+				case GLFW_KEY_EQUAL:
+					makeWindow();
 					break;
 			}
 			break;
@@ -81,26 +112,26 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
 	}
 }
 void cursorPositionCallback(GLFWwindow* window, double xpos, double ypos) { // TODO: get window class from callback instead of GLFWwindow
-	glfwSetCursorPos(demo::window1->getWindow(), 0, 0);
-	if (demo::window1->getInputMode(GLFW_CURSOR) != GLFW_CURSOR_DISABLED)
+	glfwSetCursorPos(demo::wincams[0].window->getWindow(), 0, 0);
+	if (demo::wincams[0].window->getInputMode(GLFW_CURSOR) != GLFW_CURSOR_DISABLED)
 		return;
 	glm::vec2 mouseDelta(xpos, ypos);
 	mouseDelta *= -demo::mouseSens;
-	demo::camera1->transform->yaw(mouseDelta.x);
-	demo::camera1->transform->pitch(mouseDelta.y);
-	demo::camera1->transform->setPitch(std::clamp(demo::camera1->transform->getPitch(), -90.0f, 90.0f));
+	demo::wincams[0].camera->transform->yaw(mouseDelta.x);
+	demo::wincams[0].camera->transform->pitch(mouseDelta.y);
+	demo::wincams[0].camera->transform->setPitch(std::clamp(demo::wincams[0].camera->transform->getPitch(), -90.0f, 90.0f));
 }
 void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) { // TODO: get window class from callback instead of GLFWwindow
 	if (action == GLFW_PRESS)
-		if (demo::window1->getInputMode(GLFW_CURSOR) != GLFW_CURSOR_DISABLED) {
-			demo::window1->setInputMode(GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-			glfwSetCursorPos(demo::window1->getWindow(), 0, 0);
+		if (demo::wincams[0].window->getInputMode(GLFW_CURSOR) != GLFW_CURSOR_DISABLED) {
+			demo::wincams[0].window->setInputMode(GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+			glfwSetCursorPos(demo::wincams[0].window->getWindow(), 0, 0);
 		}
 }
 void windowSizeCallback(GLFWwindow* window, int width, int height) { // TODO: get window class from callback instead of GLFWwindow
-	demo::window1->makeCurrent();
-	demo::renderEngine->setFramebufferSize(demo::window1->getFramebufferSize());
-	demo::camera1->projection = glm::perspective(glm::radians(75.0), (double)demo::window1->getWindowAspectRatio(), 0.1, 100.0);
+	demo::wincams[0].window->makeCurrent();
+	demo::renderEngine->setFramebufferSize(demo::wincams[0].window->getFramebufferSize());
+	demo::wincams[0].camera->projection = glm::perspective(glm::radians(75.0), (double)demo::wincams[0].window->getWindowAspectRatio(), 0.1, 100.0);
 }
 
 int main(int argc, char* argv[]) {
@@ -108,25 +139,15 @@ int main(int argc, char* argv[]) {
 
 	demo::renderEngine = new ce::RenderEngine();
 
-	demo::window1 = new ce::Window("Cinnabar1");
-	demo::window1->makeCurrent();
-	demo::renderEngine->vsync(0);
-	demo::renderEngine->setFramebufferSize(demo::window1->getFramebufferSize());
-	demo::window2 = new ce::Window("Cinnabar2");
-	demo::window2->makeCurrent();
-	demo::renderEngine->vsync(0);
-	demo::renderEngine->setFramebufferSize(demo::window2->getFramebufferSize());
-	
 	double deltaTimeMin = 1.0 / 1000.0; // framerate cap
 
-	demo::camera1 = new ce::Camera();
-	demo::camera1->projection = glm::perspective(glm::radians(75.0), (double)demo::window1->getWindowAspectRatio(), 0.1, 100.0);
-	demo::camera2 = new ce::Camera(demo::camera1->transform);
-	demo::camera2->projection = glm::perspective(glm::radians(75.0), (double)demo::window1->getWindowAspectRatio(), 0.1, 100.0);
-	demo::camera1->transform->setPosition(0.0f, 0.0f, 1.5f);
+	demo::cameraTransform = new ce::Transform();
+	demo::cameraTransform->setPosition(0.0f, 0.0f, 1.5f);
 	demo::cameraVelocity = glm::vec3(0.0f);
 
-	demo::window1->makeCurrent();
+	makeWindow();
+
+	demo::wincams[0].window->makeCurrent();
 	// TODO: better system for setting which window meshes go to
 	ce::Mesh* blobMesh = new ce::Mesh("blob.obj");
 	ce::Material* blobMaterial = new ce::Material("matcap");
@@ -134,7 +155,6 @@ int main(int argc, char* argv[]) {
 	ce::Transform* blobPos = new ce::Transform();
 	blobPos->setScale(0.5f, 0.5f, 0.5f);
 
-	demo::window2->makeCurrent();
 	ce::Mesh* environmentMesh = new ce::Mesh("environment.obj");
 	ce::Material* environmentMaterial = new ce::Material("multitexture example", {}, 2);
 	environmentMaterial->textures[0] = new ce::Texture("color.png");
@@ -142,15 +162,17 @@ int main(int argc, char* argv[]) {
 	ce::Transform* environmentPos = new ce::Transform();
 	environmentPos->setPosition(0.0f, -1.0f, 0.0f);
 
-	if (glfwRawMouseMotionSupported()) // TODO: make function for all these GLFW functions and figure out for multiple windows
-		demo::window1->setInputMode(GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
-	glfwSetKeyCallback(demo::window1->getWindow(), keyCallback);
-	glfwSetCursorPosCallback(demo::window1->getWindow(), cursorPositionCallback);
-	glfwSetMouseButtonCallback(demo::window1->getWindow(), mouseButtonCallback);
-	glfwSetWindowSizeCallback(demo::window1->getWindow(), windowSizeCallback);
 
+	while (true) {
+		for (std::vector<demo::WinCam>::iterator i = demo::wincams.begin(); i < demo::wincams.end(); i++)
+			if (i->window->shouldClose()) {
+				delete i->window;
+				delete i->camera;
+				demo::wincams.erase(i);
+			}
+		if (demo::wincams.size() == 0)
+			goto close_program;
 
-	while (!(demo::window1->shouldClose() || demo::window2->shouldClose())) {
 		demo::time->update();
 		std::cout << "fps: " << demo::time->getFPS() << std::endl;
 
@@ -163,34 +185,32 @@ int main(int argc, char* argv[]) {
 
 		// Move camera
 		glm::vec3
-			cameraRight = demo::camera1->transform->getRight(true, false, false),
+			cameraRight = demo::cameraTransform->getRight(true, false, false),
 			cameraUp = glm::vec3(0.0f, 1.0f, 0.0f),
-			cameraFront = demo::camera1->transform->getForward(true, false, false);
-		demo::camera1->transform->translate(
+			cameraFront = demo::cameraTransform->getForward(true, false, false);
+		demo::cameraTransform->translate(
 			(cameraRight * demo::cameraVelocity.x) +
 			(cameraUp * demo::cameraVelocity.y) +
 			(cameraFront * demo::cameraVelocity.z));
 
-		// Render
-		demo::window1->makeCurrent();
-		demo::renderEngine->clear(ce::COLOR_BUFFER_BIT | ce::DEPTH_BUFFER_BIT);
-		demo::renderEngine->render(blobMesh, blobMaterial, blobPos, demo::camera1);
-		demo::window2->makeCurrent();
-		demo::renderEngine->clear(ce::COLOR_BUFFER_BIT | ce::DEPTH_BUFFER_BIT);
-		demo::renderEngine->render(environmentMesh, environmentMaterial, environmentPos, demo::camera2);
+		for (demo::WinCam wincam : demo::wincams) {
+			// render
+			wincam.window->makeCurrent();
+			demo::renderEngine->clear(ce::COLOR_BUFFER_BIT | ce::DEPTH_BUFFER_BIT);
+			demo::renderEngine->render(blobMesh, blobMaterial, blobPos, wincam.camera);
+			demo::renderEngine->render(environmentMesh, environmentMaterial, environmentPos, wincam.camera);
+			wincam.window->swapBuffers();
 
-		demo::window1->swapBuffers();
-		demo::window2->swapBuffers();
-
-		// error check
-		demo::window1->makeCurrent();
-		demo::renderEngine->errorCheck();
-		demo::window2->makeCurrent();
-		demo::renderEngine->errorCheck();
+			// error check
+			demo::renderEngine->errorCheck();
+		}
 
 		// framerate cap
 		demo::time->waitUntilDelta(deltaTimeMin);
 	}
+
+	close_program:
+
 	delete blobMesh;
 	blobMaterial->deleteContents();
 	delete blobMaterial;
@@ -201,12 +221,13 @@ int main(int argc, char* argv[]) {
 	delete environmentMaterial;
 	delete environmentPos;
 
-	delete demo::camera1->transform;
-	delete demo::camera2;
-	delete demo::camera1;
+	for (demo::WinCam wincam : demo::wincams) {
+		delete wincam.window;
+		delete wincam.camera;
+	}
+	demo::wincams.clear();
 
-	delete demo::window1;
-	delete demo::window2;
+	delete demo::cameraTransform;
 	delete demo::renderEngine;
 
 	delete demo::time;
